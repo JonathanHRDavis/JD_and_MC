6ca603a42f445d23e7c84b892ce485260ec07940
mwcormier42 <mwcormier42@students.tntech.edu>
2015-01-29 11:33:39 -0600

    getting a lot of compiler errors

--- ReadFile.h
+++ ReadFile.h
@@ -1,22 +1,24 @@
-#if !defined READ_FILE
-#define READ_FILE
+#if !defined READ_H
+#define READ_H
 
 #include "Text.h"
 
 #include <fstream>
 using namespace std;
-
-struct ReadFile
+//switched it to a class
+class ReadFile
 {
-   ifstream input_file;
-   bool _eof;
-   bool closed;
+	private:
+		ifstream input_file;
+		bool _eof;
+		bool closed;
+	public:
+		ReadFile(const char* file_name);
+		~ReadFile();
+		
+		String* readLine();
+		bool eof();
+		void close();
 };
-
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
 
 #endif
